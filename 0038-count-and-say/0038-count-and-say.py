45ffa516e6165d40bbd2af8ca475cecbe52ce499
Vatsal Vora <vatsaliit30@gmail.com>
2022-10-13 01:04:30 +0530

    Time: 122 ms (9.91%), Space: 14.6 MB (7.70%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mapFunc(self, s):
+        ans = []
+        p = s[0]
+        c = 1
+        for i in range(1, len(s)):
+            if (s[i] == p):
+                c+=1
+            else:
+                ans.append([str(c), p])
+                p = s[i]
+                c = 1
+        ans.append([str(c), p])
+        print(ans)
+        return ans
+
+    def countAndSay(self, n: int) -> str:
+        if (n==1):
+            return "1"
+        else:
+            return ''.join([item for sublist in self.mapFunc(self.countAndSay(n-1)) for item in sublist])
+        
