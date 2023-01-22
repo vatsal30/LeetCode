8db4ed9a37e3097e24c96001314df592345a6fd1
Vatsal Vora <vatsaliit30@gmail.com>
2023-01-23 00:21:27 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
@@ -1,34 +1,61 @@
 #User function Template for python3
 
-
+from collections import deque
 class Solution:
     
-    def dfs(self, node, V, adj, vis, path):
-        vis[node] = 1
-        path[node] = 1
+    # using DFS
+    # def dfs(self, node, V, adj, vis):
+    #     vis[node] = 2
         
-        for i in adj[node]:
-            if vis[i]==0:
-                if self.dfs(i, V, adj, vis, path):
-                    return True
-            elif path[i]==1:
-                return True
+    #     for i in adj[node]:
+    #         if vis[i]==0:
+    #             if self.dfs(i, V, adj, vis):
+    #                 return True
+    #         elif vis[i]==2:
+    #             return True
         
-        path[node] = 0
-        return False
+    #     vis[node] = 1
+    #     return False
     
-    #Function to detect cycle in a directed graph.
-    def isCyclic(self, V, adj):
-        # code here
-        vis = [0 for i in range(V)]
-        path = [0 for i in range(V)]
-        for i in range(V):
-            if (vis[i]==0):
-                if (self.dfs(i, V, adj, vis, path)):
-                    return True
+    # #Function to detect cycle in a directed graph.
+    # def isCyclic(self, V, adj):
+    #     # code here
+    #     vis = [0 for i in range(V)]
+    #     for i in range(V):
+    #         if (vis[i]==0):
+    #             if (self.dfs(i, V, adj, vis)):
+    #                 return True
         
-        return False
-
+    #     return False
+    
+    
+    # Using BFS Kahn's algo
+    def isCyclic(self, V, adj):
+        indegree = [0 for i in range(V)]
+        for i in range(V):
+            for node in adj[i]:
+                indegree[node] += 1
+        
+        que = deque()
+        
+        for i in range(V):
+            if indegree[i] == 0:
+                que.append(i)
+        
+        cnt = 0
+        
+        while que:
+            node = que.popleft()
+            cnt += 1
+            for ele in adj[node]:
+                indegree[ele] -= 1
+                if indegree[ele] == 0:
+                    que.append(ele)
+        
+        if (cnt == V): return False
+        
+        return True
+    
 
 #{ 
  # Driver Code Starts
