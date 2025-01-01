a96df51c812737074edba8e91910bd8bb196b302
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-01 17:22:35 +0530

    Time: 0 ms (100%), Space: 17.7 MB (11.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,5 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [-1] * n
-        def dfs(idx):
+    def dfs(self, idx, dp, nums):
             if idx == 0:
                 dp[idx] = nums[idx]
                 return nums[idx]
@@ -10,9 +7,19 @@ class Solution:
                 return 0
             if dp[idx] != -1:
                 return dp[idx]
-            left = dfs(idx-2) + nums[idx]
-            right = dfs(idx-1)
+            left = self.dfs(idx-2, dp, nums) + nums[idx]
+            right = self.dfs(idx-1, dp, nums)
             dp[idx] = max(left, right)
             return dp[idx]
-        dfs(n-1)
-        return dp[n-1]
+
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        adj_house = nums[0]
+        non_adj = 0
+        for i in range(1, n):
+            pick = nums[i] + non_adj
+            not_pick = adj_house
+            curr_pick = max(pick, not_pick)
+            non_adj = adj_house
+            adj_house = curr_pick
+        return adj_house
