4fd3925fd93c44a918193db870ea6e5f0d8aacd6
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-06 00:55:13 +0530

    Time: 1 ms (100%), Space: 17.8 MB (100%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.py
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.py
@@ -3,7 +3,7 @@ class Solution:
         pat = p.split('*')
         if pat[0] not in s:
             return False
-        idx1 = s.index(pat[0]) if pat[0] else 0
+        idx1 = s.index(pat[0])
         if pat[1] in s[idx1 + len(pat[0]):]:
             return True
         return False
