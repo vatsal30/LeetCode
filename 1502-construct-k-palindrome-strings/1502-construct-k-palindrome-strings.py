88f62281837ec51d58b4d8f845fc43375e5968d5
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-11 19:35:48 +0530

    Time: 6 ms (100%), Space: 18.1 MB (34.36%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -2,6 +2,9 @@ class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         if len(s) < k:
             return False
+        # This is because there are only 26 letters, so a max of 26 odd occurences.
+        if k >= 26:
+            return True
         freq_map = Counter(s)
         oneCount = 0
         for letter in freq_map:
