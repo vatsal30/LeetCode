ae75fb71110cdc5507681f902d04f64cd19add0b
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-11 18:44:15 +0530

    Time: 35 ms (44.49%), Space: 18.1 MB (34.36%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        freq_map = Counter(s)
+        oneCount = 0
+        print(freq_map)
+        for letter in freq_map:
+            if freq_map[letter]%2 == 1:
+                oneCount += 1
+        if oneCount > k:
+            return False
+        return True
+        # oneCount = sum(i % 2 == 1 for i in range)
