6daeccedb01182f5f48a3c1f07701a32aa81524a
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-12 22:35:15 +0530

    Time: 0 ms (100%), Space: 17.8 MB (27%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openStack = []
+        starStack = []
+        for idx, letter in enumerate(s):
+            if letter == '(':
+                openStack.append(idx)
+            elif letter == '*':
+                starStack.append(idx)
+            elif letter == ')' and openStack:
+                openStack.pop()
+            elif letter == ')' and starStack:
+                starStack.pop()
+            else:
+                return False
+        while openStack and starStack and openStack[-1] < starStack[-1]:
+            openStack.pop()
+            starStack.pop()
+        print(openStack)
+        print(starStack)
+        if openStack:
+            return False
+        return True
