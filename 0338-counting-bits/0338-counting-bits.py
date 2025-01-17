c03cdff86f46d4561cc2a95b3d6c93588730c9a6
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-17 16:55:55 +0530

    Time: 3 ms (94.98%), Space: 18.4 MB (51.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,13 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
+    def countBitsWithoutBit(self, n: int) -> List[int]:
+        ## DP Solution without bit maniplation
         ans = [0]
         for i in range(1, n + 1):
             ans.append(i % 2 + ans[i // 2])
         return ans
 
-        
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = (i & 1) + dp[i >> 1]
+        return dp
