454e4b350fc8045e05083c496d81c95e485d9a25
Vatsal Vora <vatsaliit30@gmail.com>
2025-01-22 17:26:18 +0530

    Time: 53 ms (82.1%), Space: 51.6 MB (70.03%) - LeetHub

---
+++ 2864-is-object-empty/2864-is-object-empty.ts
@@ -0,0 +1,8 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    // return !Object.keys(obj).length;
+    for (const _ in obj) return false;
+    return true;
+};
