64459c5ac41877568a6aa5416f5589bd8d32e6f8
Vatsal Vora <vatsaliit30@gmail.com>
2025-02-01 11:31:22 +0530

    Time: 0 ms (100%), Space: 17.9 MB (12.54%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -1,10 +1,9 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        parityArr = [1 if num % 2 == 0 else 0 for num in nums]
         n = len(nums)
         if n == 1:
             return True
         for idx in range(1, n):
-            if parityArr[idx-1] == parityArr[idx]:
+            if nums[idx-1] % 2 == nums[idx] % 2:
                 return False
         return True
