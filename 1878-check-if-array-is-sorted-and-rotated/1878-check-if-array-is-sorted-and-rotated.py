f2f9e787ebbfb31a73d22f8b68555c2c188923c6
Vatsal Vora <vatsaliit30@gmail.com>
2025-02-02 17:05:04 +0530

    Time: 0 ms (100%), Space: 17.7 MB (70.45%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,17 +1,15 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
         flag = True
-        rotateIdx = -1
+        rotateIdx = True
         for idx in range(1, len(nums)):
             if nums[idx] >= nums[idx-1]:
                 continue
             elif flag and nums[idx] < nums[idx-1]:
-                rotateIdx = idx
+                rotateIdx = False
                 flag = False
             else:
                 return False
-        if rotateIdx == -1:
+        if not rotateIdx and nums[-1] <= nums[0]:
             return True
-        if rotateIdx != -1 and nums[-1] <= nums[0]:
-            return True
-        return False
+        return rotateIdx
