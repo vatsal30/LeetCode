c551c977ed43a23ea672c7936cf970cae762499d
Vatsal Vora <vatsaliit30@gmail.com>
2025-02-09 16:28:58 +0530

    Time: 146 ms (67.02%), Space: 27.2 MB (60.69%) - LeetHub

--- 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.py
@@ -12,7 +12,7 @@ class Solution:
         ans = 0
         mod = 10**9 + 7
         for num in nums:
-            diff = num - self.reverse_number(num)
+            diff = num - int(str(num)[::-1])
             diff_map[diff] += 1
             ans = (ans + diff_map[diff] - 1) % mod
         return ans
