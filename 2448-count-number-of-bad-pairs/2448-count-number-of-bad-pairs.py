f82e32a7325da357f133608b8aed69d7fa457361
Vatsal Vora <vatsaliit30@gmail.com>
2025-02-09 22:52:09 +0530

    Time: 57 ms (95.95%), Space: 39.4 MB (8.1%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -3,8 +3,8 @@ class Solution:
         ans = 0
         n = len(nums)
         diff = Counter([idx - nums[idx] for idx in range(n)])
-        total = n * (n-1) // 2
+        bad = n * (n-1) // 2
         good = 0
         for good_pair in diff.values():
-            good += (good_pair) * (good_pair - 1) // 2
-        return total - good
+            bad -= (good_pair) * (good_pair - 1) // 2
+        return bad
