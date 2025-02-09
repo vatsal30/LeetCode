647bc735b4cc544cd757a95f28f91c0d0646f06e
Vatsal Vora <vatsaliit30@gmail.com>
2025-02-09 16:28:06 +0530

    Time: 236 ms (11.34%), Space: 27.4 MB (41.69%) - LeetHub

---
+++ 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse_number(self, n):
+        r = 0
+        while n > 0:
+            r *= 10
+            r += n % 10
+            n //= 10
+        return r
+
+    def countNicePairs(self, nums: List[int]) -> int:
+        diff_map = defaultdict(int)
+        ans = 0
+        mod = 10**9 + 7
+        for num in nums:
+            diff = num - self.reverse_number(num)
+            diff_map[diff] += 1
+            ans = (ans + diff_map[diff] - 1) % mod
+        return ans
+
+
