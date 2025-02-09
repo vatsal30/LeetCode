fae756f179c38dccd2089faf1dcae066f085c9fb
Vatsal Vora <vatsaliit30@gmail.com>
2025-02-09 22:49:02 +0530

    Time: 99 ms (28.09%), Space: 38.8 MB (59.29%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -2,11 +2,8 @@ class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         ans = 0
         diff = defaultdict(int)
-        n = len(nums)
-        total = n * (n-1) // 2
-        good = 0
         for idx, num in enumerate(nums):
             curr_diff = num - idx
             diff[curr_diff] += 1
-            good += (diff[curr_diff] - 1) 
-        return total - good
+            ans += (idx + 1) - diff[curr_diff]
+        return ans
