df919c56c1e6ae1179f606d6ecd764cd9a200cdc
Vatsal Vora <vatsaliit30@gmail.com>
2025-04-13 18:36:34 +0530

    Time: 72 ms (26.99%), Space: 17.7 MB (52.65%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,9 +1,11 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        freq = Counter(nums)
-        sorted_freq = sorted(freq.items())
-        if sorted_freq[0][0] < k:
-            return -1
-        return len(sorted_freq) if sorted_freq[0][0] > k else len(sorted_freq) - 1
+        ans = 0
+        for num in set(nums):
+            if num < k:
+                return -1
+            elif num > k:
+                ans += 1
+        return ans
             
         
