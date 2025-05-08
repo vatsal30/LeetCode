a2f16341296ed87cc7b6e177e6a2b67cdf5054c0
Vatsal Vora <vatsaliit30@gmail.com>
2025-05-08 23:37:39 +0530

    Time: 3608 ms (15.45%), Space: 115.7 MB (46.35%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -6,14 +6,14 @@ class Solution:
         time_cost = (1, 2)
         end = (len(moveTime) - 1, len(moveTime[0]) - 1)
         DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        visited = set()
+        visited = [[0] * len(moveTime[0]) for _ in range(len(moveTime))]
         while que:
             time, x, y, time_cost_idx = heappop(que)
             if (x, y) == end:
                 return time 
-            if (x, y, time_cost_idx) in visited:
+            if visited[x][y]:
                 continue
-            visited.add((x, y, time_cost_idx))
+            visited[x][y] = 1
             for direction in DIRECTIONS:
                 nx, ny = x + direction[0], y + direction[1]
                 if 0 <= nx <= end[0] and 0 <= ny <= end[1]:
