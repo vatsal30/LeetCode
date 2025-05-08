39a75c910b681b6b223f8e4d8061697a033406f4
Vatsal Vora <vatsaliit30@gmail.com>
2025-05-08 23:42:38 +0530

    Time: 156 ms (26.51%), Space: 18.4 MB (51.97%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,25 @@
+from heapq import heappush, heappop
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        que = []
+        heappush(que, (0, 0, 0))
+        end = (len(moveTime) - 1, len(moveTime[0]) - 1)
+        DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        visited = [[0] * len(moveTime[0]) for _ in range(len(moveTime))]
+        while que:
+            time, x, y = heappop(que)
+            if (x, y) == end:
+                return time 
+            if visited[x][y]:
+                continue
+            visited[x][y] = 1
+            for direction in DIRECTIONS:
+                nx, ny = x + direction[0], y + direction[1]
+                if 0 <= nx <= end[0] and 0 <= ny <= end[1]:
+                    new_time = max(time, moveTime[nx][ny]) + 1
+                    # new_time_cost = 2 if time_cost == 1 else 1
+                    heappush(que, (new_time, nx, ny))
+            
+
+
+        
