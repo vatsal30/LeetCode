2b251116f78750d277ef1a89ddc29a81d28cdfa2
Vatsal Vora <vatsaliit30@gmail.com>
2025-05-08 23:40:34 +0530

    Time: 3629 ms (15.02%), Space: 116.9 MB (46.35%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -2,13 +2,12 @@ from heapq import heappush, heappop
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
         que = []
-        heappush(que, (0, 0, 0, 0))
-        time_cost = (1, 2)
+        heappush(que, (0, 0, 0, 1))
         end = (len(moveTime) - 1, len(moveTime[0]) - 1)
         DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         visited = [[0] * len(moveTime[0]) for _ in range(len(moveTime))]
         while que:
-            time, x, y, time_cost_idx = heappop(que)
+            time, x, y, time_cost = heappop(que)
             if (x, y) == end:
                 return time 
             if visited[x][y]:
@@ -17,8 +16,9 @@ class Solution:
             for direction in DIRECTIONS:
                 nx, ny = x + direction[0], y + direction[1]
                 if 0 <= nx <= end[0] and 0 <= ny <= end[1]:
-                    new_time = max(time, moveTime[nx][ny]) + time_cost[time_cost_idx] 
-                    heappush(que, (new_time, nx, ny, (time_cost_idx + 1) % 2))
+                    new_time = max(time, moveTime[nx][ny]) + time_cost
+                    new_time_cost = 2 if time_cost == 1 else 1
+                    heappush(que, (new_time, nx, ny, new_time_cost))
             
 
 
