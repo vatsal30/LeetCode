3e21bb75b4ba751d8e51dcb3747d62363e448ced
Vatsal Vora <vatsaliit30@gmail.com>
2026-01-05 22:31:12 +0530

    Time: 31 ms (10.4%), Space: 19.9 MB (95.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_track = {}
+        traker = {}
         for value in strs:
-            sorted_value = ''.join(sorted(value))
-            if sorted_value in anagram_track:
-                anagram_track[sorted_value].append(value)
+            arr = [0]*26
+            for val in value:
+                arr[ord(val) - 97] += 1
+            sig = ""
+            for idx, val in enumerate(arr):
+                if val > 0:
+                    sig += chr(idx + 97)*val
+            if sig in traker:
+                traker[sig].append(value)
             else:
-                anagram_track[sorted_value] = [value]
-        return list(anagram_track.values())
+                traker[sig] = [value]
+        return list(traker.values())
+        # n*k(logk)
+        # anagram_track = {}
+        # for value in strs:
+        #     sorted_value = ''.join(sorted(value))
+        #     if sorted_value in anagram_track:
+        #         anagram_track[sorted_value].append(value)
+        #     else:
+        #         anagram_track[sorted_value] = [value]
+        # return list(anagram_track.values())
         
