cd93cf115a4f3efdb08a97258e47147367ff563a
Vatsal Vora <vatsaliit30@gmail.com>
2026-01-05 22:17:02 +0530

    Time: 13 ms (54.7%), Space: 19.9 MB (95.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_track = {}
-        for idx, value in enumerate(strs):
+        for value in strs:
             sorted_value = ''.join(sorted(value))
             if sorted_value in anagram_track:
                 anagram_track[sorted_value].append(value)
