cb9151d66b6ddb1f75137a51ae56d75afe60448f
Vatsal Vora <vatsaliit30@gmail.com>
2026-01-05 22:01:44 +0530

    Time: 15 ms (42.04%), Space: 17.7 MB (81.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,16 @@
-from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        track_char = {}
         if len(s) != len(t):
             return False
-        s_dict = defaultdict(int)
-        for c in s:
-            s_dict[c] += 1
-        for c in t:
-            if s_dict[c] > 0:
-                s_dict[c] -= 1
+        for char in s:
+            if char in track_char:
+                track_char[char] += 1
+            else:
+                track_char[char] = 1
+        for char in t:
+            if char in track_char and track_char[char] > 0:
+                track_char[char] -= 1
             else:
                 return False
         return True
