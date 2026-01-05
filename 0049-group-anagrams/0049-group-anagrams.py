07cc051265d10aa17d4e31f2bfd4a7dcd585c17d
Vatsal Vora <vatsaliit30@gmail.com>
2026-01-05 22:15:49 +0530

    Time: 11 ms (82.85%), Space: 19.9 MB (95.16%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_track = {}
+        for idx, value in enumerate(strs):
+            sorted_value = ''.join(sorted(value))
+            if sorted_value in anagram_track:
+                anagram_track[sorted_value].append(value)
+            else:
+                anagram_track[sorted_value] = [value]
+        return list(anagram_track.values())
+        
