fde6ab07ffccb7c467d5874f5283db4e09d5d318
Vatsal Vora <vatsaliit30@gmail.com>
2026-01-05 21:50:20 +0530

    Time: 0 ms (100%), Space: 18.4 MB (73.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
+        checker = {}
         for idx, num in enumerate(nums):
             diff = target - num
-            if diff in num_dict:
-                return [num_dict[diff], idx]
+            if diff in checker:
+                return [checker[diff], idx]
             else:
-                num_dict[num] = idx
-        # for idx, num in enumerate(nums):
-        #     diff = target - num
-        #     if diff in num_dict:
-        #         if diff == num and len(num_dict[diff]) > 1:
-        #             return num_dict[diff]
-        #         elif diff != num:
-        #             return [idx, num_dict[diff][0]]
-                
-
-            
+                checker[num] = idx
