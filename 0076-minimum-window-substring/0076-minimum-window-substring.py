ee34076a74e2539f8826e69d1cbc732256ee2b2b
Vatsal Vora <vatsaliit30@gmail.com>
2026-02-12 13:56:16 +0530

    Time: 59 ms (87.18%), Space: 19.7 MB (48.83%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,24 +1,21 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        required = len(t)
-        if required > len(s):
+        if len(t) > len(s):
             return ""
-        t_map = defaultdict(int)
-        for c in t:
-            t_map[c] += 1
-        right = 0
-        left = 0
+        t_map = Counter(t)
+        required = len(t_map)
+        left, right = 0, 0
         min_len = float("inf")
         min_left_idx = 0
-        matched = 0
+        formed = 0
         s_map = defaultdict(int)
         while right < len(s):
             right_char = s[right]
             s_map[right_char] += 1
             if s_map[right_char] == t_map[right_char]:
-                matched += t_map[right_char]
-            if matched == required:
+                formed += 1
+            if formed == required:
                 while left <= right:
                     left_char = s[left]
                     if right-left+1 < min_len:
@@ -27,7 +24,7 @@ class Solution:
                     s_map[left_char] -= 1
                     left += 1
                     if s_map[left_char] < t_map[left_char]:  
-                        matched -= t_map[left_char]
+                        formed -= 1
                         break
             right += 1
         return "" if min_len == float("inf") else s[min_left_idx:min_left_idx+min_len]
