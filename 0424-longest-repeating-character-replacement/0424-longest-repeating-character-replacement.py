6557ae8a64931ae3bbc3106a5af0ffde3a8a6ac6
Vatsal Vora <vatsaliit30@gmail.com>
2026-03-22 13:34:10 +0530

    Time: 53 ms (99.65%), Space: 19.4 MB (99.11%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,16 @@
 from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        max_freq = 0
-        left = 0
         freq = defaultdict(int)
-        
+        left = 0
+        max_char = 0
+        right = 0
         for right in range(len(s)):
             freq[s[right]] += 1
+            max_char = max(max_char, freq[s[right]])
 
-            # We only care about maximum frequency occured in array
-            max_freq = max(freq[s[right]], max_freq)
-            if (right - left + 1 - max_freq) > k:
-                freq[s[left]]-=1
+            if (right - left + 1) - max_char > k:
+                freq[s[left]] -= 1
                 left += 1
-
+            
         return right - left + 1
