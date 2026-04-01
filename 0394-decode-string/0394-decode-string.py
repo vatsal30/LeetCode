b32808181718a5b1acf41b4e314ca1cde6d900e0
Vatsal Vora <vatsaliit30@gmail.com>
2026-04-01 16:02:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (60.41%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        cs = []
+        ds = []
+        num = 0
+        es = ""
+        for c in s:
+            if c == '[':
+                cs.append(es)
+                ds.append(num)
+                num = 0
+                es = ""
+            elif c.isdigit():
+                num = num * 10 + int(c)
+            elif c == "]":
+                ps, cnt = cs.pop(), ds.pop()
+                es = ps + cnt*es
+            else:
+                es += c
+        print(cs, es)
+        return ''.join(cs) + es
