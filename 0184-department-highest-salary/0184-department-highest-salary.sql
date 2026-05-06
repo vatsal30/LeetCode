ebdcafaca29a7cad5b2f61d6747f7fa6788ba76e
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-06 22:49:49 +0530

    Time: 847 ms (32.95%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+WITH employe_salary_rank AS (
+    SELECT
+        e.name AS Employee,
+        d.name AS Department,
+        e.salary AS Salary,
+        Rank() OVER (
+            PARTITION BY d.name
+            ORDER BY e.salary DESC
+        ) AS salary_rank
+    FROM Employee AS e 
+    JOIN Department as d WHERE e.departmentId = d.id
+)
+
+SELECT Department, Employee, Salary
+FROM employe_salary_rank
+WHERE salary_rank = 1
+ORDER BY Salary DESC
+
