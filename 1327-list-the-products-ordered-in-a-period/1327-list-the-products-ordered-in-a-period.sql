e0a72db00532345a29f00a9f8d7eac48575034f9
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-10 14:07:23 +0530

    Time: 1064 ms (6.18%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -6,4 +6,4 @@ FROM Products AS p
 JOIN Orders AS o
     ON p.product_id = o.product_id AND o.order_date >= '2020-02-01' AND o.order_date < '2020-03-01'
 GROUP BY p.product_id
-HAVING SUM(o.unit) >= 100
+HAVING unit >= 100
