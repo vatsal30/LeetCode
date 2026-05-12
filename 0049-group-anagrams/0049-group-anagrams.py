1a04c991a4aa5ff8ee50dc119f4ff4fde49d2f4e
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-12 22:06:05 +0530

    Time: 15 ms (44.92%), Space: 24 MB (9.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,11 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         group_dict = defaultdict(list)
         for s in strs:
             cnt = [0] * 26
             for c in s:
-                cnt[ord(c) - 97] += 1
+                cnt[ord(c) - ord('a')] += 1
             group_dict[tuple(cnt)].append(s)
         
         return list(group_dict.values())
