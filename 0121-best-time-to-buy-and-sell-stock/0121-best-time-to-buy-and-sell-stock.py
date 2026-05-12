48998e20e837c91571f34b622c68e0de21ae397f
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-12 21:55:45 +0530

    Time: 46 ms (62.07%), Space: 28.6 MB (42.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left = 0
         profit = 0
-        for right in range(1, len(prices)):
-            if prices[left] > prices[right]:
-                left = right
+        buy = prices[0]
+
+        for i in range(1, len(prices)):
+            if prices[i] > buy:
+                profit = max(profit, prices[i] - buy)
             else:
-                profit = max(profit, prices[right] - prices[left])
+                buy = prices[i]
         return profit
