e87d4efba33b070842a8e6a0518c6f6c1946f154
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-12 22:04:46 +0530

    Time: 15 ms (44.92%), Space: 24 MB (18.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,26 +1,11 @@
+from collections import Counter, defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        traker = {}
-        for value in strs:
-            arr = [0]*26
-            for val in value:
-                arr[ord(val) - 97] += 1
-            sig = ""
-            for idx, val in enumerate(arr):
-                if val > 0:
-                    sig += chr(idx + 97)*val
-            if sig in traker:
-                traker[sig].append(value)
-            else:
-                traker[sig] = [value]
-        return list(traker.values())
-        # n*k(logk)
-        # anagram_track = {}
-        # for value in strs:
-        #     sorted_value = ''.join(sorted(value))
-        #     if sorted_value in anagram_track:
-        #         anagram_track[sorted_value].append(value)
-        #     else:
-        #         anagram_track[sorted_value] = [value]
-        # return list(anagram_track.values())
+        group_dict = defaultdict(list)
+        for s in strs:
+            cnt = [0] * 26
+            for c in s:
+                cnt[ord(c) - 97] += 1
+            group_dict[tuple(cnt)].append(s)
         
+        return list(group_dict.values())
