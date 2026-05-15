6e678543e13ed7d97d632e37e65a54132703c510
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-15 16:13:59 +0530

    Time: 0 ms (100%), Space: 20.5 MB (75.52%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,11 +3,11 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (right + left) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < target:
-                left = mid + 1
-            else:
+            if nums[mid] > target:
                 right = mid - 1
+            else:
+                left = mid + 1
         return -1
