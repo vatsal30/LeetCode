662470ad90180831153edee50fff97f989cdded6
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-15 15:57:57 +0530

    Time: 0 ms (100%), Space: 19.2 MB (96.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (right + left) // 2
             if nums[mid] == target:
                 return mid
             if nums[left] <= nums[mid]:
@@ -16,6 +16,4 @@ class Solution:
                     left = mid + 1
                 else:
                     right = mid - 1
-
-        return -1
-            
+        return -1  
