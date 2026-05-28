a78b734d8edb8d311c841877d2503e7f4a14fa5c
Vatsal Vora <vatsaliit30@gmail.com>
2026-05-28 23:22:52 +0530

    Time: 20 ms (91.68%), Space: 19.5 MB (63.13%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -8,9 +8,9 @@ class Solution:
     def treeChecker(self, node1, node2):
         if not node1 or not node2:
             return node1 == node2
-        if node1.val == node2.val:
-            return self.treeChecker(node1.left, node2.left) and self.treeChecker(node1.right, node2.right)
-        return False
+        if node1.val != node2.val:
+            return False
+        return self.treeChecker(node1.right, node2.right) and self.treeChecker(node1.left, node2.left)
  
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not root:
