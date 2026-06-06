c6ff12b1b899d12078c93944ea7a3b7185928bb3
Vatsal Vora <vatsaliit30@gmail.com>
2026-06-06 16:51:16 +0530

    Time: 51 ms (61.49%), Space: 20.4 MB (61.61%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,41 +10,56 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        leftmost = root
-        while leftmost:
-            curr = leftmost
-            leftmost = None
+        curr = root
+        while curr:
+            dummy = Node()
+            tail = dummy
             while curr:
-                next_n = curr.next
-                if curr.left:    
-                    if not leftmost:
-                        leftmost = curr.left
-                    if curr.right:
-                        curr.left.next = curr.right
-                    else:
-                        while next_n:
-                            if next_n.left:
-                                curr.left.next = next_n.left
-                                break
-                            elif next_n.right:
-                                curr.left.next = next_n.right
-                                break
-                            else:
-                                next_n = next_n.next
+                if curr.left:
+                    tail.next = curr.left
+                    tail = tail.next
                 if curr.right:
-                    if not leftmost:
-                        leftmost = curr.right
-                    while next_n:
-                        if next_n.left:
-                            curr.right.next = next_n.left
-                            break
-                        elif next_n.right:
-                            curr.right.next = next_n.right
-                            break
-                        else:
-                            next_n = next_n.next
-                curr = next_n
+                    tail.next = curr.right
+                    tail = tail.next
+                curr = curr.next
+            curr = dummy.next
         return root
+        # Initial solution scan forward approach
+        # leftmost = root
+        # while leftmost:
+        #     curr = leftmost
+        #     leftmost = None
+        #     while curr:
+        #         next_n = curr.next
+        #         if curr.left:    
+        #             if not leftmost:
+        #                 leftmost = curr.left
+        #             if curr.right:
+        #                 curr.left.next = curr.right
+        #             else:
+        #                 while next_n:
+        #                     if next_n.left:
+        #                         curr.left.next = next_n.left
+        #                         break
+        #                     elif next_n.right:
+        #                         curr.left.next = next_n.right
+        #                         break
+        #                     else:
+        #                         next_n = next_n.next
+        #         if curr.right:
+        #             if not leftmost:
+        #                 leftmost = curr.right
+        #             while next_n:
+        #                 if next_n.left:
+        #                     curr.right.next = next_n.left
+        #                     break
+        #                 elif next_n.right:
+        #                     curr.right.next = next_n.right
+        #                     break
+        #                 else:
+        #                     next_n = next_n.next
+        #         curr = next_n
+        # return root
             
 
         
