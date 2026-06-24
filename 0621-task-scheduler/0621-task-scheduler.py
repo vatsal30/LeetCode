2d29dab793a62d7e3ed063c16de147fc16487e89
Vatsal Vora <vatsaliit30@gmail.com>
2026-06-24 16:00:44 +0530

    Time: 19 ms (83.47%), Space: 20.6 MB (96.31%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,24 +1,28 @@
-from collections import deque
-import heapq
+from collections import Counter
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counts = [0] * 26
-        for task in tasks:
-            counts[ord(task) - ord('A')] += 1
+        counts = Counter(tasks)
+        max_freq = max(counts.values())
+        max_counts = sum(1 for count in counts.values() if count == max_freq)
+        return max(len(tasks), (max_freq - 1) * (n + 1) + max_counts)
+        # Heap Task schedular approach
+        # counts = [0] * 26
+        # for task in tasks:
+        #     counts[ord(task) - ord('A')] += 1
 
-        counts = [-val for val in counts if val > 0]
-        heapq.heapify(counts)
-        interval = 0
-        queue = deque()
-        while counts or queue:
-            interval += 1
-            while queue and queue[0][0] < interval:
-                heapq.heappush(counts, queue.popleft()[1])
-            if counts:
-                freq = heapq.heappop(counts)
-                if freq + 1 < 0:
-                    queue.append((interval + n, freq+1))
-        return interval
+        # counts = [-val for val in counts if val > 0]
+        # heapq.heapify(counts)
+        # interval = 0
+        # queue = deque()
+        # while counts or queue:
+        #     interval += 1
+        #     while queue and queue[0][0] < interval:
+        #         heapq.heappush(counts, queue.popleft()[1])
+        #     if counts:
+        #         freq = heapq.heappop(counts)
+        #         if freq + 1 < 0:
+        #             queue.append((interval + n, freq+1))
+        # return interval
             
             
