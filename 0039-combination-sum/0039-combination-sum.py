b1979d1b48edc9ee53ce596c6e86086ae6694fa9
Vatsal Vora <vatsaliit30@gmail.com>
2026-07-30 00:27:44 +0530

    Time: 7 ms (76.93%), Space: 19.5 MB (64.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,15 @@
 class Solution:
-    def findCombinations(self, ind, candidates, target, combinations, ds):
-        if (ind == len(candidates)):
-            if (target == 0):
-                combinations.append(ds.copy())
-            return 
-        if (candidates[ind] <= target):
-            ds.append(candidates[ind])
-            self.findCombinations(ind, candidates, target-candidates[ind], combinations, ds)
-            ds.pop(-1)
-        self.findCombinations(ind+1, candidates, target, combinations, ds)
-
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        combinations = []
-        self.findCombinations(0, candidates, target, combinations, [])
-        return combinations
+        result, combination = [], []
+        def backtrack(idx, target):
+            if target == 0:
+                result.append(combination[:])
+                return
+            elif target < 0:
+                return
+            for i in range(idx, len(candidates)):  
+                combination.append(candidates[i])
+                backtrack(i, target - candidates[i])
+                combination.pop()
+        backtrack(0, target)
+        return result
