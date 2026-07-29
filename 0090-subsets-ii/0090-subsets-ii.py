01d65fe6d5b247fbdcc835c6f90654eb2dbe7b0d
Vatsal Vora <vatsaliit30@gmail.com>
2026-07-30 00:05:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (58.75%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,22 +1,15 @@
 class Solution:
-    def generateSubset(self, idx, arr, subsets, subset):
-        if (idx == len(arr)):
-            # subsets.append(subset.copy())
-            return
-        for i in range(idx, len(arr)):
-            if (i>idx and arr[i]==arr[i-1]):
-                continue
-            
-            subset.append(arr[i])
-            subsets.append(subset.copy())
-            self.generateSubset(i+1, arr, subsets, subset)
-            subset.pop(-1)
-            
-            # self.generateSubset(i+1, arr, subsets, subset)
-            
-            
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        subsets = [[]]
         nums.sort()
-        self.generateSubset(0, nums, subsets, [])
-        return subsets
+        result, subset = [], []
+        def backtrack(idx):
+            
+            result.append(subset[:])
+            for i in range(idx, len(nums)):
+                if i > idx and nums[i] == nums[i-1]:
+                    continue
+                subset.append(nums[i])
+                backtrack(i + 1)
+                subset.pop()
+        backtrack(0)
+        return result
