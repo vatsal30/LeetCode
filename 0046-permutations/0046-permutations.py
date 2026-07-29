c6e97448de1201da812537a9b5c47dc4450cad6b
Vatsal Vora <vatsaliit30@gmail.com>
2026-07-30 00:35:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (65.27%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,14 @@
 class Solution:
-    def findPermute(self, idx, nums, answer):
-        if (idx == len(nums)):
-            answer.append(nums.copy())
-            return
-        # print(answer)
-        for i in range(idx, len(nums)):
-            nums[idx], nums[i] = nums[i], nums[idx]
-            self.findPermute(idx+1, nums, answer)
-            nums[idx], nums[i] = nums[i], nums[idx]
-            
-                
     def permute(self, nums: List[int]) -> List[List[int]]:
-        answer = []
-        self.findPermute(0, nums, answer)
-        return answer
-        
+        result = []
+        def backtrack(perm):
+            if len(perm) == len(nums):
+                result.append(perm[:])
+                return
+            for num in nums:
+                if num not in perm:
+                    perm.append(num)
+                    backtrack(perm)
+                    perm.pop()
+        backtrack([])
+        return result
