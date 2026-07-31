453cd5376fef5e7832e2907a5e2cbfe9c9bbec0e
Vatsal Vora <vatsaliit30@gmail.com>
2026-07-31 21:25:29 +0530

    Time: 54 ms (16.37%), Space: 33.6 MB (65.83%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,26 +1,16 @@
 class Solution:
-    def isPalindrom(self, str):
-        left = 0
-        right = len(str)-1
-        while (left <= right):
-            if (str[left] != str[right]):
-                return False
-            left +=1
-            right -= 1
-        return True
+    def isPalindrom(self, s: str) -> bool:
+        return s == s[::-1]
 
-    def doPartition(self, s, idx, ans, ds):
-        if (idx == len(s)):
-            ans.append(ds.copy())
-            return
-        
-        for i in range(idx, len(s)):
-            if (self.isPalindrom(s[idx:(i+1)])):
-                ds.append(s[idx:i+1])
-                self.doPartition(s, i+1, ans, ds)
-                ds.pop()
-            
     def partition(self, s: str) -> List[List[str]]:
-        ans = []
-        self.doPartition(s, 0, ans, [])
-        return ans
+        result, perm = [], []
+        def backtrack(start):
+            if len(s) == start:
+                result.append(perm[:])
+            for i in range(start, len(s)):
+                if self.isPalindrom(s[start:i+1]):
+                    perm.append(s[start:i+1])
+                    backtrack(i + 1)
+                    perm.pop()
+        backtrack(0)
+        return result
