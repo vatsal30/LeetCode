8f6c6f9814b2f4b62f3d77c05e110da2e285b931
Vatsal Vora <vatsaliit30@gmail.com>
2026-08-02 17:43:27 +0530

    Time: 3 ms (89.71%), Space: 22.7 MB (94.13%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,34 +1,17 @@
+import heapq
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_freq = {}
+        num_freq = defaultdict(int)
         for num in nums:
-            num_freq[num] = 1 + num_freq.get(num, 0)
-        
-        bucket_arr = [[] for _ in range(len(nums) + 1)]
-
-        for num in num_freq:
-            bucket_arr[num_freq[num]].append(num)
-        
+            num_freq[num] += 1
+        ranked = []
+        for num, freq in num_freq.items():
+            heapq.heappush(ranked, (freq, num))
+            if len(ranked) > k:
+                heapq.heappop(ranked)
         ans = []
-        for i in range(len(bucket_arr) - 1, 0, -1):
-            for num in bucket_arr[i]:
-                ans.append(num)
-                if len(ans) == k:
-                    return ans
- 
-        # max_cnt = 0
-        # cnt = {}
-        # for num in nums:
-        #     if num in cnt:
-        #         cnt[num] += 1
-        #         max_cnt = max(max_cnt, cnt[num])
-        #     else:
-        #         cnt[num] = 1
-        #         max_cnt = max(max_cnt, cnt[num])
-        # ans = []
-        # while len(ans) != k:
-        #     for val in cnt:
-        #         if cnt[val] == max_cnt:
-        #             ans.append(val)
-        #     max_cnt -= 1
-        # return ans
+        while ranked:
+            ans.append(heapq.heappop(ranked)[1])
+
+        return ans
